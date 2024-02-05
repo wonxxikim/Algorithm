d1b4e7b790cd519cc41f3976a7afa25e9bfed2c7
wonxxikim <102503653+wonxxikim@users.noreply.github.com>
2024-02-05 14:19:00 +0900

    Update 요세푸스 문제.java

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -13,21 +13,21 @@ public class Main{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());  //N명의 사람
+		int K = Integer.parseInt(st.nextToken());  //K번째 사람 제거
 		StringBuilder sb = new StringBuilder();
 		sb.append("<");
 		for(int i = 1; i<N+1 ; i++) {
-			q.add(i);
+			q.add(i);  //사람(큐) 초기화
 		}
-		while(q.size()!=1) {
-			for(int i = 0 ; i<K-1 ; i++) {
+		while(q.size()!=1) {  //한명 남을 때 까지 반복
+			for(int i = 0 ; i<K-1 ; i++) { //K번째 전까지 큐의 뒤로 넣어주기
 				q.add(q.poll());
 			}
-			sb.append(q.poll()).append(", ");
+			sb.append(q.poll()).append(", "); //K번째에서는 결과값에 append
 			
 		}
-		sb.append(q.poll()).append(">");
+		sb.append(q.poll()).append(">"); //남은 한명 append
 		System.out.println(sb);
 	}
 
